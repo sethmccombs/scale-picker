81e9df43223c0dcce40f185562bbe4be73db033d
Seth McCombs <smccombs@triller.co>
2019-03-24 21:23:07 -0700

    Add return at end

--- scale-picker.py
+++ scale-picker.py
@@ -7,11 +7,10 @@
 import random as rand
 
 positions = ["A", "A#", "Bb", "B", "B#", "Cb", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab"  ]
-
+root_note = positions[rand.randint(0, len(positions)-1)]
 
 # List of Modes
 modes = ["ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "locrian"]
-print(modes)
 
 # ionian =
 # dorian =
@@ -22,22 +21,30 @@ print(modes)
 # locrian =
 
 # Check for voice (Ab vs G#)
-
-
-
 # Prompt if playing 7 or 6 string
 
-
+# seven_string = input("Are you playing a seven string? ")
+seven_string = "no"
 
 # Select Mode
 # mode = modes[random.randint(1, len(modes)]
 mode = modes[rand.randint(0, len(modes)-1)]
-print (mode)
+
 # Select Root Note
 
-
-
 # Select Root String
+strings = []
+
+if seven_string.lower() == 'yes':
+    strings = ["5th","6th","7th"]
+elif seven_string.lower() == 'no':
+    strings = ["5th","6th"]
+
+
+string = strings[rand.randint(0, len(strings)-1)]
+
+
+print("%s %s, starting on the %s string" % (root_note, mode, string))
 
 
 
