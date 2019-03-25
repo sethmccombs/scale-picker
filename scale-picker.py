e17e1ef0372fb81220adeb84295d775588a2365e
Seth McCombs <smccombs@triller.co>
2019-03-24 19:09:54 -0700

    initial commit

---
+++ scale-picker.py
@@ -0,0 +1,7 @@
+# Program to randomly choose a root note, and scale to play based on the modes
+# Ionian, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian
+# Will return whether to play the 6th or 5th string root
+# Will prompt at the beginning to choose 7 or 6 string guitar
+# If playing a 7 string, will return 7th, 6th, or 5th string root
+
+
