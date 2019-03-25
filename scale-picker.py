a275e76cc34423c6abab86d869b096468a0719d8
Seth McCombs <smccombs@triller.co>
2019-03-24 20:33:54 -0700

    pick random mode

--- scale-picker.py
+++ scale-picker.py
@@ -4,20 +4,33 @@
 # Will prompt at the beginning to choose 7 or 6 string guitar
 # If playing a 7 string, will return 7th, 6th, or 5th string root
 
+import random as rand
 
 # List of Modes
 modes = ["ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "locrian"]
 print(modes)
 
+
 # List of Root Notes
+
 
 # Check for voice (Ab vs G#)
 
+
+
 # Prompt if playing 7 or 6 string
 
-# Select Mode
 
+
+# Select Mode
+# mode = modes[random.randint(1, len(modes)]
+print(modes)
+print(modes[rand.randint(0, len(modes)-1)])
 # Select Root Note
 
+
+
 # Select Root String
+
+
 
